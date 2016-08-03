6e087fc6990bce563dfb6a17cf737ea40d2f153a
zak <kazboddington@gmail.com>
2016-08-03 17:54:06 +0100

    Create new project with javaFX set up and working

---
+++ PacketConstructor/src/packetconstructor/Header.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package packetconstructor;
+
+import java.util.ArrayList;
+
+public abstract class Header {
+    private String name;
+    private int numberOfFields;
+    private ArrayList<Field> fields;
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setNumberOfFields(int i) {
+        this.numberOfFields = i;
+    }
+
+    void addField(Field destinationMac) {
+        this.fields.add(destinationMac);
+    }
+    
+}
